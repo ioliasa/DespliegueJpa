193ed2de12225f3dbbade34479012b9b8efc3646
Estudiante <ignmmartin@gmail.com>
2022-02-15 09:21:42 +0000

    Correcion 2.0

--- src/test/java/com/example/demo/ProyectoJpaSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/ProyectoJpaSpringBootApplicationTests.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.demo.model.Usuario;
 
 @SpringBootTest
 class ProyectoJpaSpringBootApplicationTests {
+	
+	private static Usuario user;
+	 @BeforeAll
+	    public static void init() {
+	        user = new Usuario();
+	    }
 
 	@Test
 	void testGetDireccion() {
